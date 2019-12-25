689ac4448ec734f620b3b71470fba57c7ceb60c6
刘铭浩 <liuminghao@tuzhan.com>
2019-12-25 11:20:23 +0800

    更新

---
+++ src/components/components/wrapper/PopupWrapper.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="popup-wrapper">
+    <slot></slot>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.popup-wrapper {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+}
+</style>
